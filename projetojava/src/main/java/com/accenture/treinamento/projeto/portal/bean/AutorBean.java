65c0a5696f36931c1ea78349138c1a8d5ac427f0
thaaalesr <lorgusprogramming@gmail.com>
2017-05-21 17:02:19 -0300

    AutorBean

---
+++ projetojava/src/main/java/com/accenture/treinamento/projeto/portal/bean/AutorBean.java
@@ -0,0 +1,26 @@
+package com.accenture.treinamento.projeto.portal.bean;
+
+public class AutorBean {
+
+	
+	private Integer codigo;
+	private String nome;
+	
+	
+	public Integer getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
+		
+}
