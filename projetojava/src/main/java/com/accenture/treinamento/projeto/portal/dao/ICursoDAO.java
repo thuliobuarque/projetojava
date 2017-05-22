c7fa59dd88cb91301e54a49d4144f97058371158
veridianaMelo <veridianadasilvameloxxi@gmail.com>
2017-05-22 15:59:42 -0300

    [Codificação da Interface ICursoDAO]

--- projetojava/src/main/java/com/accenture/treinamento/projeto/portal/dao/ICursoDAO.java
+++ projetojava/src/main/java/com/accenture/treinamento/projeto/portal/dao/ICursoDAO.java
@@ -1,5 +1,11 @@
 package com.accenture.treinamento.projeto.portal.dao;
 
 public interface ICursoDAO {
+	
+	public abstract boolean cadastrarCurso(CursoBean curso) throws ProjetoException;
+	public abstract boolean alterarCurso(CursoBean curso) throws ProjetoException;
+	public abstract boolean excluirCurso(CursoBean curso) throws ProjetoException;
+	public abstract List listaCurso() throws ProjetoException;
+
 
 }
