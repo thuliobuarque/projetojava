5fe2fceee1b0c32febe41ffe992ace7b3b751e07
thulio <thulio.buarque2@gmail.com>
2017-05-22 23:40:53 -0300

    interface de autor

--- projetojava/src/main/java/com/accenture/treinamento/projeto/livraria/dao/IAutorDAO.java
+++ projetojava/src/main/java/com/accenture/treinamento/projeto/livraria/dao/IAutorDAO.java
@@ -1,9 +1,15 @@
 package com.accenture.treinamento.projeto.livraria.dao;
 
+import java.util.List;
+
 import com.accenture.treinamento.projeto.exception.ProjetoException;
 import com.accenture.treinamento.projeto.livraria.model.AutorBean;
 
 public interface IAutorDAO {
 
 	public abstract boolean cadastrarAutor(AutorBean autor) throws ProjetoException;
+	public abstract boolean alterarAutor(AutorBean autor) throws ProjetoException;
+	public abstract boolean excluirAutor(AutorBean autor) throws ProjetoException;
+	public abstract List listaAutor() throws ProjetoException;
+	
 }
