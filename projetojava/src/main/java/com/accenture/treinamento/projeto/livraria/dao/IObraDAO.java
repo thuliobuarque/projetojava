b6c937d528bd1d5f9c073121774ee4553e4bfbaa
thulio <thulio.buarque2@gmail.com>
2017-05-22 23:38:28 -0300

    interface de obra

--- projetojava/src/main/java/com/accenture/treinamento/projeto/livraria/dao/IObraDAO.java
+++ projetojava/src/main/java/com/accenture/treinamento/projeto/livraria/dao/IObraDAO.java
@@ -1,5 +1,15 @@
 package com.accenture.treinamento.projeto.livraria.dao;
 
-public interface IObraDAO {
+import java.util.List;
 
+import com.accenture.treinamento.projeto.exception.ProjetoException;
+import com.accenture.treinamento.projeto.livraria.model.ObraBean;
+
+public interface IObraDAO {
+	
+	public abstract boolean cadastrarObra(ObraBean usuario) throws ProjetoException;
+	public abstract boolean alterarObra(ObraBean usuario) throws ProjetoException;
+	public abstract boolean excluirObra(ObraBean usuario) throws ProjetoException;
+	public abstract List listaObra() throws ProjetoException;
+	
 }
